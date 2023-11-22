82f7bbba1a01204a430d66e45e7418c670465a78
Rob Thomas <robt84@gmail.com>
2023-11-22 12:07:42 -0600

    added bootstrap

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
-import { Inter } from 'next/font/google'
+import 'bootstrap/dist/css/bootstrap.css';
 import './globals.css'
-
+import { Inter } from 'next/font/google'
+import BootstrapClient from './components/BootstrapClient'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <BootstrapClient />
+      </body>
     </html>
   )
 }
