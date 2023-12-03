7d2ef7821e646fd54e63cbe3f8612497efbdbea3
Rob Thomas <robt84@gmail.com>
2023-12-03 10:11:38 -0600

    adding in the header and footer

--- src/app/page.js
+++ src/app/page.js
@@ -3,20 +3,12 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main>
-      <div className="text-center mt-4 col-md-6 mx-auto"> 
-        <h1 className="text-danger mb-5">Hello Bootstrap</h1>
-        <div className='mb-5'>
-        <p>
-        <Link className='mb-5' href="/company/about-us">About us</Link>
-        </p>
-        <p>
-        <Link href="/company/approach">Approach</Link>
-        </p>
-        <p>
-          <Link href="/company/partnership">Partnership</Link>
-        </p>
-        </div>
-      </div>
+      <h1>Empuls3</h1>
+      <p>
+        <Link href="/company/about">
+         About Us
+        </Link>
+      </p>
     </main>
   )
 }
